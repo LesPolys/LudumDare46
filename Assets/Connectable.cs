537814b4b0f036b616de02067041b3330f8ced50
lespolys <stewart.jlt@gmail.com>
2020-04-18 17:25:21 -0400

    Connection and linking
    altered prefabs to have a connection point and wrote some logic to connect them and lock them in place on release.
    Small occasional bug in the linked peice may not always have an accurate convex hull after linking. Robot can be picked up after as a whole :)

---
+++ Assets/Connectable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connectable : MonoBehaviour
+{
+    [SerializeField]
+    public Transform connectionPoint;
+
+    public bool isConnected = false;
+
+}
--- Assets/Scripts/Hands.cs
+++ Assets/Scripts/Hands.cs
@@ -31,16 +31,33 @@ public class Hands : MonoBehaviour
         {
             if (hit.collider.tag == "Pickup")
             {
+                Debug.Log("Pickup");
+
                 if (Input.GetMouseButtonDown(0))
                 {
-                
-                    isGrabbing = true;
-                    grabbedObject = hit.collider.gameObject;
-                    hit.transform.SetParent(holdPoint, true);
-                    hit.rigidbody.useGravity = false;
-                    hit.rigidbody.isKinematic = true;
-                    hit.transform.position = holdPoint.position;
-                    //hit.collider.enabled = false;
+
+                    if (!hit.transform.gameObject.GetComponent<Connectable>().isConnected)
+                    {
+                        isGrabbing = true;
+                        grabbedObject = hit.collider.gameObject;
+                        hit.transform.SetParent(holdPoint, true);
+                        hit.rigidbody.useGravity = false;
+                        hit.rigidbody.isKinematic = true;
+                        hit.transform.position = holdPoint.position;
+                        hit.collider.enabled = false;
+                    }
+                    else
+                    {
+                        isGrabbing = true;
+                        grabbedObject = hit.collider.gameObject.transform.root.gameObject;
+                        grabbedObject.transform.SetParent(holdPoint, true);
+                        grabbedObject.GetComponent<Rigidbody>().useGravity = false;
+                        grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
+                        grabbedObject.transform.position = holdPoint.position;
+                        grabbedObject.GetComponent<Collider>().enabled = false;
+                    }
+
+       
 
                     // hit.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
                     //Debug.Log("GRAB");
@@ -51,21 +68,47 @@ public class Hands : MonoBehaviour
             {
                 UnHighlight();
             }
+
+            Debug.DrawLine(cameraTransform.position + cameraTransform.forward * offsetDistance, hit.point, Color.red);
+
         }
         else
         {
+            Debug.DrawRay(cameraTransform.position + cameraTransform.forward * offsetDistance, cameraTransform.forward * maxGrabDistance, Color.green);
+
             UnHighlight();
         }
 
 
         if (Input.GetMouseButtonUp(0))
         {
-           // Debug.Log("Release");
+            if (Physics.Raycast(cameraTransform.position + cameraTransform.forward * offsetDistance, cameraTransform.forward, out hit, maxGrabDistance))
+            {
+                if (hit.collider.tag == "Pickup" && grabbedObject != null && !hit.transform.gameObject.GetComponent<Connectable>().isConnected  && !grabbedObject.GetComponent<Connectable>().isConnected)
+                {
+                    isGrabbing = false;
+                    Transform connectionPoint = hit.transform.gameObject.GetComponent<Connectable>().connectionPoint.transform;
+                    grabbedObject.transform.SetParent(connectionPoint);
+                    grabbedObject.GetComponent<Collider>().enabled = true;
+
+                    grabbedObject.transform.position = connectionPoint.position;
+                    grabbedObject.transform.rotation = connectionPoint.rotation;
+
+                    hit.transform.gameObject.GetComponent<Connectable>().isConnected = true;
+                    grabbedObject.GetComponent<Connectable>().isConnected = true;
+
+                    grabbedObject = null;
+                }
+                Debug.DrawLine(cameraTransform.position + cameraTransform.forward * offsetDistance, hit.point, Color.red);
+
+            }
+
+            // Debug.Log("Release");
             if (grabbedObject != null && isGrabbing)
             {
                     isGrabbing = true;
                     grabbedObject.GetComponent<Rigidbody>().useGravity = true;
-                    //grabbedObject.GetComponent<Collider>().enabled = true;
+                    grabbedObject.GetComponent<Collider>().enabled = true;
                     grabbedObject.transform.SetParent(null);
                     grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
                     //grabbedObject.transform.localScale = new Vector3(1, 1, 1);
@@ -79,7 +122,7 @@ public class Hands : MonoBehaviour
             HandleHighlighting(grabbedObject);
         }
 
-        Debug.DrawRay(cameraTransform.position + cameraTransform.forward * offsetDistance, cameraTransform.forward * maxGrabDistance, Color.green);
+
     }
 
     void HandleHighlighting(GameObject newHighlightObject)
