418eb29ab3eaea98210768aa78d134f05228f033
lespolys <stewart.jlt@gmail.com>
2020-04-18 11:47:53 -0400

    Initial commit

---
+++ Assets/Scripts/Hands.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hands : MonoBehaviour
+{
+    [SerializeField]
+    Transform cameraTransform;
+
+    [SerializeField]
+    Transform holdPoint;
+
+    [SerializeField]
+    float offsetDistance;
+
+    [SerializeField]
+    float maxGrabDistance;
+
+    private GameObject grabbedObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("CLICK");
+            RaycastHit hit;
+            if (Physics.Raycast(cameraTransform.position + cameraTransform.forward * offsetDistance, cameraTransform.forward, out hit, maxGrabDistance))
+            {
+                if (hit.collider.tag == "Pickup")
+                {
+                    Debug.Log("GRAB");
+                }
+            }
+        }
+        Debug.DrawRay(cameraTransform.position + cameraTransform.forward * offsetDistance, cameraTransform.forward * maxGrabDistance, Color.green);
+    }
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    private float xRotation = 0f;
+
+    void Awake()
+    {
+        //Cursor.visible = false;
+        //Screen.lockCursor = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
