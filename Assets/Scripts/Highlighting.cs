a286f9ec5c31b47885fa381744f565b633277768
lespolys <stewart.jlt@gmail.com>
2020-04-18 16:23:20 -0400

    Workshop base
    updated highlighting
    workshop prefab and scene made

--- Assets/Scripts/Highlighting.cs
+++ Assets/Scripts/Highlighting.cs
@@ -12,13 +12,49 @@ public class Highlighting : MonoBehaviour
 
     public void Highlight()
     {
-        gameObject.GetComponent<MeshRenderer>().material.shader = highLightShader;
+      
+       if (gameObject.GetComponent<MeshRenderer>())
+       {
+           //gameObject.GetComponent<MeshRenderer>().material.shader = highLightShader;
+            Material[] mats = gameObject.GetComponent<MeshRenderer>().materials;
+            foreach (Material mat in mats)
+            {
+                mat.shader = highLightShader;
+            }
+       }
+
+       foreach(MeshRenderer render in gameObject.GetComponentsInChildren<MeshRenderer>())
+       {
+           // render.material.shader = highLightShader;
+            Material[] mats = render.materials;
+            foreach (Material mat in mats)
+            {
+                mat.shader = highLightShader;
+            }
+        }
     }
 
     public void UnHighlight()
     {
-        gameObject.GetComponent<MeshRenderer>().material.shader = normalShader;
+        if (gameObject.GetComponent<MeshRenderer>())
+        {
+           // gameObject.GetComponent<MeshRenderer>().material.shader = normalShader;
+            Material[] mats = gameObject.GetComponent<MeshRenderer>().materials;
+            foreach (Material mat in mats)
+            {
+                mat.shader = normalShader;
+            }
+        }
+
+        foreach (MeshRenderer render in gameObject.GetComponentsInChildren<MeshRenderer>())
+        {
+            //render.material.shader = normalShader;
+
+            Material[] mats = render.materials;
+            foreach (Material mat in mats)
+            {
+                mat.shader = normalShader;
+            }
+        }
     }
-
-
 }
